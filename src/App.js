cb59dfef2749e61e4a599c42a2f7b07d087134f6
QurbanovaLamiya <lamiyaqurbanova121@gmail.com>
2022-10-25 17:54:21 +0400

    Update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Component, Fragment } from "react";
+import "./App.css";
+import FormContent from "./components/FormContent";
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <FormContent />
+      </Fragment>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FormContent/index.js
@@ -0,0 +1,10 @@
+import { Component, Fragment } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+class FormContent extends Component {
+  render() {
+    return <Fragment></Fragment>;
+  }
+}
+
+export default FormContent;
